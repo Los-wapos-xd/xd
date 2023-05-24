e32931635c4c77d31a61d88293ad141353530965
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-24 09:11:55 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Square,
+    5000,
+    0,
+    255,
+    0,
+    9999,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
 })
+input.onButtonPressed(Button.AB, function () {
+    music.setBuiltInSpeakerEnabled(true)
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.setBuiltInSpeakerEnabled(false)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+})
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # . .
+    `)
+basic.pause(1000)
+basic.clearScreen()
